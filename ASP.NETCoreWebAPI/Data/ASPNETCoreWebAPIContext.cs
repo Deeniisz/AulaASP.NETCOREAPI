64b4d9c1a35b2bcc4fea398db5ab5b0c92caa972
Deeniisz <assis.denis98@gmail.com>
2021-09-17 08:47:18 -0300

    Aula

---
+++ ASP.NETCoreWebAPI/Data/ASPNETCoreWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASP.NETCoreWebAPI.Model;
+
+namespace ASP.NETCoreWebAPI.Data
+{
+    public class ASPNETCoreWebAPIContext : DbContext
+    {
+        public ASPNETCoreWebAPIContext (DbContextOptions<ASPNETCoreWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP.NETCoreWebAPI.Model.Person> Person { get; set; }
+    }
+}
